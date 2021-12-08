8eb343332498429bc51edf595e88ffb53d9eb453
Austin Schmidli <austin.schmidli@gmail.com>
2021-12-07 19:27:12 -0700

    feat: initialize apps

--- src/service.ts
+++ src/service.ts
@@ -1,13 +1,23 @@
+import express from 'express';
 import { readdir, readJSON } from 'fs-extra';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 import { join } from 'path';
+import { resolver } from './resolve';
+
+export type ServiceConfig = {
+  port: string;
+  proxy: {
+    host: string;
+  };
+};
 
 type ServiceConfigMap = {
-  [key: string]: any;
+  [key: string]: ServiceConfig;
 };
 
 export const serviceConfigMap: ServiceConfigMap = {};
 
-export async function populate(servicesDir: string) {
+export async function populateConfig(servicesDir: string) {
   const services = await readdir(servicesDir);
 
   // Load all service configs in parallel.
@@ -20,4 +30,34 @@ export async function populate(servicesDir: string) {
   await Promise.all(configs);
 
   console.log({ serviceConfigMap });
+}
+
+export function initializeApp(name: string, config: ServiceConfig) {
+  const { port, proxy } = config;
+  const app = express();
+
+  app.use('*', resolver(name, config));
+
+  if (proxy) {
+    app.use(
+      '*',
+      createProxyMiddleware({
+        target: proxy.host,
+        changeOrigin: true,
+      })
+    );
+  }
+
+  app.listen(port);
+  console.log(`Listening on ${port}`);
+
+  return app;
+}
+
+export async function startServices() {
+  await populateConfig('./moxxy');
+
+  const apps = Object.keys(serviceConfigMap).map((service) =>
+    initializeApp(service, serviceConfigMap[service])
+  );
 }
